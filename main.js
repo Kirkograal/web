bcea4c23a131c46967be56ab1d69c6c7087ff532
Kirko <kirkograal@gmail.com>
2017-05-18 13:42:12 -0500

    Simple app

---
+++ main.js
@@ -0,0 +1,24 @@
+var express = require('express'),
+	app     = express();
+
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+	ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+
+console.log(`PORT ${process.env.PORT}`);
+console.log(`IP ${process.env.IP}`);
+console.log(`OPENSHIFT_NODEJS_PORT ${process.env.OPENSHIFT_NODEJS_PORT}`);
+console.log(`OPENSHIFT_NODEJS_IP ${process.env.OPENSHIFT_NODEJS_IP}`);
+
+app.get('/', (req, res) => {
+	res.send("Hey im working!!");
+	res.status(200);
+});
+
+app.use((err, req, res, next) => {
+	console.error(err.stack);
+	res.status(500).send('Oops');
+});
+
+app.listen(port, ip, () => {
+	console.log(`Server running on http://${ip}:${port}`);
+});
