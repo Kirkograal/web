8e85e918a62531f2ebb8f550c7f3c3a90f50ff67
Kirko <kirkograal@gmail.com>
2017-05-27 22:18:59 -0500

    updates trying to get webhost to fking work

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router  = express.Router();
 
 router.get('/', (req, res) => {
-	res.send('In development');
+	res.send('Site is in development');
 	res.status(200);
 });
 
